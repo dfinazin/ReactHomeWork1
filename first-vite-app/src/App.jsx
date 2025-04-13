e1c4a1abda96dcde9bbad426b6c2eac63c16a779
dfinazhin <dfinazhin@gmail.com>
2025-04-14 01:51:45 +0500

    Without JSX

--- first-vite-app/src/App.jsx
+++ first-vite-app/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, createElement } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-export const App = () => {               // < Императивный
-  const [count, setCount] = useState(0)  // Декларативный >
-  const currentDate = new Date();
+export const App = () => {
+    // < Императивный
+    const [count, setCount] = useState(0); // Декларативный >
+    const currentDate = new Date();
+    const viteLogoImg = createElement("img", {
+        src: viteLogo,
+        className: "logo",
+        alt: "Vite logo",
+    });
+    const viteLink = createElement(
+        "a",
+        { href: "https://vite.dev", target: "_blank" },
+        viteLogoImg
+    );
+    const reactLogoImg = createElement("img", {
+        src: reactLogo,
+        className: "logo react",
+        alt: "React logo",
+    });
+    const reactLink = createElement(
+        "a",
+        { href: "https://react.dev", target: "_blank" },
+        reactLogoImg
+    );
+    const images = createElement("div", {}, viteLink, reactLink);
+    const header = createElement("h1", {}, "Vite + React");
+    const card = createElement(
+        "div",
+        { className: "card" },
+        createElement(
+            "button",
+            { onClick: () => setCount((count) => count + 1) },
+            `count is ${count}`
+        ),
+        createElement(
+            "p",
+            {},
+            "Edit ",
+            createElement("code", {}, "src/App.jsx"),
+            " and save to test HMR"
+        )
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p> {currentDate.getFullYear()} </p>
-    </>
-  )
-}
+    const mainConteiner = createElement(
+        "div",
+        {},
+        images,
+        header,
+        card,
+        createElement(
+            "p",
+            { className: "read-the-docs" },
+            "Click on the Vite and React logos to learn more"
+        ),
+        createElement("p", {}, currentDate.getFullYear())
+    );
+
+    return mainConteiner;
+};
